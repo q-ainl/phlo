6d72a8b8fa376717a381ab7da14118ed4625fccb
q-devnl <jordi@q-dev.nl>
2026-02-22 03:43:28 +0100

    Rename tech.php to phlo.php

--- preload.php
+++ preload.php
@@ -1,6 +1,6 @@
 <?php
 if (function_exists('opcache_compile_file')){
-	opcache_compile_file(__DIR__.'/tech.php');
+	opcache_compile_file(__DIR__.'/phlo.php');
 	opcache_compile_file(__DIR__.'/build.php');
 	opcache_compile_file(__DIR__.'/constants.php');
 	opcache_compile_file(__DIR__.'/debug.php');
